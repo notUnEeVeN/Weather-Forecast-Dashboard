850c81f78ac65c509f557c0ed399bcc71a31f4a3
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-18 11:24:32 -0700

    added the second api call that uses the lat and lon from the first one to get weather information of lcoation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
 $(document).ready(function(){
     $('#search-button').on('click', function() {
         var inputValue = $('#search-input').val();
+
+        var apiKey = "2c9bcb2e033ed7202d80b7f29fc73541";
         
         var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?" + 
-        "q=" + inputValue + "&limit=5&appid=2c9bcb2e033ed7202d80b7f29fc73541";
+        "q=" + inputValue + "&limit=5&appid=" + apiKey;
 
         fetch(apiUrl)
         .then(function (response) {
             if (!response.ok) {
-                console.error('Network response was not ok');
+                console.error('Network response failed');
                 return;
             }
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            var secondApiUrl = "http://api.openweathermap.org/data/2.5/forecast?" +
+            "lat=" + lat +
+            "&lon=" + lon +
+            "&appid=" + apiKey;
+
+            fetch(secondApiUrl)
+            .then(function(response) {
+                if (!response.ok) {
+                    console.error('Network response failed')
+                }
+                return response.json();
+            })
+            .then(function(secondData) {
+                console.log(secondData);
+                
+            })
         })
     });
 });
