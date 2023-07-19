f4f329c660529fc6eecee61218e739de30c34ad9
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-18 19:15:47 -0700

    added the functionality of the search history and its buttons, and comments

--- README.md
+++ README.md
@@ -1,102 +1,24 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Tybalt Mallet's Weather Forecast Dashboard
 
-## Your Task
+## Description
+This is the Module 6 challenge for the sixth week of the UCB coding bootcamp. 
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Project Link
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+## Screenshot of Website
 
-## User Story
+![Alt text](./assets/images/Screenshot%202023-07-15%20104622.jpg)
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+## Usage 
 
-## Acceptance Criteria
+This website can be used as a daily planner, you can input your hourly schedule, and get a quick reminder if you haven't completed it that time is running out! It can also be used as a way to study the basics of the concepts we learned this week such as dayjs and jquery, as well as a reminder of the JavaScript we will continue to work with in the future. 
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+## Credits 
 
-## Mock-Up
+N/A
 
-The following image shows the web application's appearance and functionality:
+## License 
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,57 @@
 $(document).ready(function () {
+    //getting the search history from local storage and setting it to a variable or an empty array
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
   var apiKey = "2c9bcb2e033ed7202d80b7f29fc73541";
+
+  renderSearchHistory()
+
+//this funciton displays the search history
+  function renderSearchHistory() {
+    //target the correct div in the html
+    var historyContainer = $("#search-history");
+    //clear buttons so they don't repeat
+    historyContainer.empty(); 
+    //iterate
+    for (var i = 0; i < searchHistory.length; i++) {
+      var historyBtn = $("<button>");
+      historyBtn.text(searchHistory[i]);
+      historyBtn.addClass("history-btn");
+      historyContainer.append(historyBtn);
+    }
+  }
   
+  //instead of storing all of the data in local storage, i run the function with the city name as a parameter again, making for less code/it updates the information and stays accurate
+  $('.history-btn').on("click", function () {
+    searchForCity($(this).text());
+  });
+
+  //search button functionality, takes inputValue and gives it to the searchforcity function
   $("#search-button").on("click", function () {
     var inputValue = $("#search-input").val();
+    //checks if there is an input value, and wether or not its included in the search history, and if so it gets pushed to the array, and set into local storage
+    if (inputValue && !searchHistory.includes(inputValue)) {
+      searchHistory.push(inputValue);
+      localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+      renderSearchHistory();
+    }
 
     if (inputValue) {
       searchForCity(inputValue);
     }
   });
 
+  //first api run to get the latitude/longitude of our cities required for the second api call
   function searchForCity(cityName) {
+    //creating the url for the first api call
     var apiUrl =
       "http://api.openweathermap.org/geo/1.0/direct?" +
       "q=" +
       cityName +
       "&limit=5&appid=" +
       apiKey;
-    fetch(apiUrl) 
+    //fetching the data
+    fetch(apiUrl)
+      //checks the status of the response, if it fails gives an error message, if it succeeds, convert into a usable object with json
       .then(function (response) {
         if (!response.ok) {
           console.error("Network response failed");
@@ -24,9 +59,11 @@ $(document).ready(function () {
         }
         return response.json();
       })
+      //putting our long and lat into variabled
       .then(function (data) {
         var lat = data[0].lat;
         var lon = data[0].lon;
+        //creating the url for the second api fetch
         var secondApiUrl =
           "http://api.openweathermap.org/data/2.5/forecast?" +
           "lat=" +
@@ -35,14 +72,18 @@ $(document).ready(function () {
           lon +
           "&appid=" +
           apiKey;
+        //second api fetch, using json to convert the data again
         fetch(secondApiUrl)
           .then(function (response) {
             return response.json();
           })
           .then(function (data) {
             console.log(data);
+            //setting the containers we want to target to variables
             var mainContainer = document.getElementById("current-weather");
-            var forecastContainer = document.getElementById("forecast-container");
+            var forecastContainer =
+              document.getElementById("forecast-container");
+            //these blocks of code call the displayweather function with the right parameters in it, and when the data from the function is returned, sets the innerHTML of our desired containers to that content
             var weatherHTML = displayWeatherData(
               data.list[0],
               data.city.name,
@@ -50,31 +91,56 @@ $(document).ready(function () {
             );
             mainContainer.style.height = "auto";
             mainContainer.innerHTML = weatherHTML;
+            // looped 5 times for the 5 smaller containers we have
             for (let i = 0; i < 5; i++) {
               var forecastHTML = displayWeatherData(
                 data.list[i * 8 + 1],
                 data.city.name,
                 false
               );
-              forecastContainer.style.height = "auto";
               forecastContainer.children[i].innerHTML = forecastHTML;
             }
           });
+        //clears our search input field
         $("#search-input").val("");
 
+        //this function extracts the wanted information from our api call, and displays it in the correct fashion
         function displayWeatherData(data, cityName, isCurrent) {
+          //the date in the api information is in a unix timestamp, so we convert it to a date we can use
           var date = dayjs.unix(data.dt);
+          //the temp degrees come in Kelvin by default when we do the api call, so i convert it to the desired Farenheit with the following formula
           var tempF = ((data.main.temp - 273.15) * 9) / 5 + 32;
+          //results in a number with a lot of decimals, this removes all but the first two
           tempF = Math.round(tempF * 10) / 10;
           var windSpeed = data.wind.speed;
           var humidity = data.main.humidity;
+          //icons require a custom link to be displayed, this line fetches the data for the proper icon from the api call, and places it into the link in the same line using the ` format
           var weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
           var dateString = date.format("MM/DD/YYYY");
+          //in order to have slightly different designs for the main container, and the smaller ones yet have them still be made in the same function, the third isCurrent parameter was added, if the isCurrent parameter is true
+          //we display it in a larger font style <h2> and include the city name
           if (isCurrent) {
-            header = "<h2>" + cityName + " (" + dateString + ')<img src="' +weatherIcon +'" alt="' + data.weather[0].description + '" /></h2>';
-          } else  {
-            header = "<h5>" + dateString + '<img src="' + weatherIcon + '" alt="' + data.weather[0].description + '" /></h5>';
+            header =
+              "<h2>" +
+              cityName +
+              " (" +
+              dateString +
+              ')<img src="' +
+              weatherIcon +
+              '" alt="' +
+              data.weather[0].description +
+              '" /></h2>';
+          } else {
+            header =
+              "<h5>" +
+              dateString +
+              '<img src="' +
+              weatherIcon +
+              '" alt="' +
+              data.weather[0].description +
+              '" /></h5>';
           }
+          //puts all our different variables together and then returns that value to the innerhtml of our containers
           var weatherDataHTML =
             header +
             "<p>Temp: " +
